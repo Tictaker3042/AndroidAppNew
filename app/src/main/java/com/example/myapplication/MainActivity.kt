0b3e9ca41a33fda03a2b8f7920eaf0f8bab77a9d
Tictaker3042 <il.lebed2011.gmail.com>
2024-02-27 13:15:26 +0300

    -38:00

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,9 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.minus1.setOnClickListener {Log.e("minus1", "MinusIsPressed1" )}
-        binding.plus1.setOnClickListener { Log.e("plus1", "PlusIsPressed1") }
-        var count = binding.number1.text
-        binding.number1.text = "65"
+        binding.buttonMinus.setOnClickListener {Log.e("buttonMinus", "MinusIsPressed1" )}
+        binding.buttonPlus.setOnClickListener { Log.e("buttonPlus", "PlusIsPressed1") }
+        var count = binding.counter.text
+        binding.counter.text = "65"
     }
 }
