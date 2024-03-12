4601be32966e8fc86d5ce956028b89f9162bd101
Tictaker3042 <il.lebed2011@gmail.com>
2024-03-12 13:23:42 +0300

    ok

--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -2,10 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var flag = 0
+        binding.button3.setOnClickListener {
+            if (flag == 0) {
+                binding.imageView.setImageResource(R.drawable.pic)
+                flag = 1
+            }
+            else {
+                binding.imageView.setImageResource(R.drawable.pic2)
+                flag = 0
+            }
+        }
     }
 }
